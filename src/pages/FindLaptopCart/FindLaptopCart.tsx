c49463d7b3707cc38ecec902e5a2c91fc2d24814
gi-carnaval <gi.carnaval@hotmail.com>
2024-06-27 10:01:33 -0300

    Updating code

--- src/pages/FindLaptopCart/FindLaptopCart.tsx
+++ src/pages/FindLaptopCart/FindLaptopCart.tsx
@@ -4,6 +4,9 @@ import { substringAndParseInt } from "../../utils/substringAndParseIntLaptopCode
 import { api } from "../../lib/axios";
 import { axiosErrorHandler } from "../../utils/axiosErrorHandler";
 import './FindLaptopCart.css'
+import { Bounce, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { RiArrowGoBackFill } from "react-icons/ri";
 
 const defaultConstraints = {
   facingMode: 'false',
@@ -20,6 +23,20 @@ const styles = {
 const FindLaptopCart = () => {
   const [qrCodeResult, setQrCodeResult] = useState<string>()
   const [cartName, setCartName] = useState<string>()
+
+  const navigate = useNavigate()
+
+  const laptopNotFound = (erro: string) => toast.error(`${erro}`, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
 
   useEffect(() => {
     if (qrCodeResult === undefined) {
@@ -38,12 +55,15 @@ const FindLaptopCart = () => {
       setCartName(cartName)
     } catch (e) {
       const errorMessage = axiosErrorHandler(e)
-      alert(`${errorMessage}`)
+      laptopNotFound(errorMessage)
     }
   }
 
   return (
     <>
+      <header className="headerBackButton">
+        <button onClick={() => navigate("..", { relative: "path" })}><RiArrowGoBackFill /> Voltar</button>
+      </header>
       <h1 className='cartPageTitle'>Encontrar Carrinho do Notebook</h1>
       <p className="infoText">
         Escaneie o Código QR do Notebook para encontrar o carrinho a que ele pertence
